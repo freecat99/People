23e120fc8c9babbf2e4b960e81ce8277c8ad73fe
adesh <adesh9744@gmail.com>
2025-09-13 02:05:03 +0530

    Fixes

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -1,23 +1,26 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom';
-import Carousel from './Carousel';
 
 function About() {
 
   const navigate = useNavigate();
   return (
-    <>
     <div className='about'>
+      <div className="main">
         <p>It's your posts.</p>
         <p>It's your profile.</p>
         <p className='special'>It's all about you.</p>
+      </div>
 
       <div className="cards">
-        <Carousel/>
+        <div className='carousel'>
+          <img src="/assets/vijay.png" alt="Vijay" className='card1'/>
+          <img src="/assets/nitin.png" alt="Nitin" className='card2' />
+          <img src="/assets/tashi.png" alt="Tashi" className='card3' />
+        </div>
         <button className="makeProfile" onClick={()=>navigate('/')}>Make your profile now!</button>
       </div>
     </div>
-    </>
   )
 }
 
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Login() {
+  const [userInfo, setUserInfo] = useState({
+    email:'',
+    password:''
+  })
+  const handleChange =(e)=>{
+    const {name, value} = e.target;
+    let copyUserInfo = {...userInfo};
+    copyUserInfo[name] = value;
+    setUserInfo(copyUserInfo);
+
+  }
+  const handleSubmit = async(e)=>{
+    e.preventDefault();
+    const {email, password} = userInfo;
+    if(!email || !password){
+    }
+    try {
+      
+    } catch (error) {
+      res.status()
+    }
+  }
   return (
-    <div>
-      login
+    <div className='container'>
+      <form>
+        <div className="entity">
+          <label htmlFor="email">Email</label>
+          <input type='email' id='email' name='email' required onChange={handleChange}/>
+        </div>
+        <div className="entity">
+          <label htmlFor="password">Password</label>
+          <input type='password' id='password' name='password' required onChange={handleChange}/>
+        </div>
+        <button onClick={handleSubmit}>Login</button>
+      </form>
+      <p><a href='/register'>Don't have an account?</a></p>
     </div>
   )
 }
